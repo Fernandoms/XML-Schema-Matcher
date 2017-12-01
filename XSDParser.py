ea76976eb4f7d802bc95fced306b64917717a70d
Fernando M Souza Filho <fernando.marques@ice.ufjf.br>
2017-12-01 04:02:57 -0200

    Modifica o XSD parser para que ele seja capaz de recuperar uma árvore equivalente ao XSD

--- XSDParser.py
+++ XSDParser.py
@@ -1,13 +1,54 @@
-import xmlschema
+from lxml import etree
+
+class XMLData(object):
+    ''' Objeto para servir de elemento na árvore que representa o XSD'''
+
+    def __init__(self, tag_name):
+        self.tag_name = tag_name
+
+    def add_occurs(self, max, min):
+        self.max_occurs = max
+        self.min_occurs = min
+
+    def add_type(self, type):
+        self.type = type
+
+    def add_path(self, path):
+        self.path = path
+
+    def add_name(self, name):
+        self.name = name
+
+
+class Node(object):
+    ''' Elemento estrutural da árvore '''
+
+    def __init__(self, tree, root):
+        self.children = []
+        self.data = Node.set_data(tree, root)
+        for child in root.getchildren():
+            self.children.append(Node(tree, child))
+
+    @staticmethod
+    def set_data(tree, element):
+        xmlData = XMLData(element.tag)
+        xmlData.add_occurs(element.get('maxOccurs'), element.get('minOccurs'))
+        xmlData.add_type(element.get('type'))
+        xmlData.add_path(tree.getpath(element))
+        xmlData.add_name(element.get('name'))
+        return xmlData
+
+    def print_tree(self, level=0):
+        print ('\t' * level + repr(self.data.tag_name), " Args ", self.data.name, self.data.max_occurs, self.data.min_occurs, self.data.type, self.data.path,  sep=' - ')
+        for child in self.children:
+            child.print_tree(level + 1)
+
 
 def demo():
-    schema = xmlschema.XMLSchema('/home/fernandoms/xml-schemas/examples/currency/BookCatalogue2.xsd')
-    elements = schema.findall('//*')
-    print (elements)
-    for element in elements:
-        print (element.name)
-        print (element.type)
+    tree = etree.parse('/home/fernandoms/Desktop/NewXMLSchema.xsd')
+    root = tree.getroot()
+    structural_tree = Node(tree, root)
+    structural_tree.print_tree()
 
-
-if __name__ == "__main__":
-    demo();
+if __name__ == '__main__':
+    demo()
--- teste.py
+++ teste.py
@@ -1,8 +1,43 @@
-from nltk.corpus import wordnet
+from lxml import etree
+import xmlschema
 
-synonyms = []
-for syn in wordnet.synsets("book"):
-    for l in syn.lemmas():
-        synonyms.append(l.name())
+class XMLData(object):
+    def __init__(self, tag_name, path, min_occurs=1, max_occurs=1, type=None, ):
+        self.min_occurs = min_occurs
+        self.max_occurs = max_occurs
+        self.tag_name = tag_name
+        self.type = type
+        self.path = path
 
-print(set(synonyms))
+
+class Node(object):
+    def __init__(self, data):
+        self.data = data
+        self.children = []
+
+    def add_child(self, obj):
+        self.children.append(obj)
+
+    def print_tree(self, level=0):
+        print ('\t' * level + repr(self.data.tag_name))
+        for child in self.children:
+            child.print_tree(level + 1)
+
+
+tree = etree.parse('/home/fernandoms/Desktop/NewXMLSchema.xsd')
+schema = xmlschema.XMLSchema('/home/fernandoms/Desktop/NewXMLSchema.xsd')
+
+
+root = tree.getroot()
+
+for el in tree.iter():
+    print(el.tag)
+
+
+
+
+'''root = tree.getroot()
+root_schema = schema.find(tree.getpath(root))
+print(root_schema)
+print(tree.getpath(root))
+root_info = XMLData(root.tag, tree.getpath(root), type=root_schema.type.name, max_occurs=root_schema.max_occurs, min_occurs=root_schema.min_occurs)'''
