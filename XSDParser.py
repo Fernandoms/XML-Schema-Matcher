d9ab24ddf460118a4b76f1db560dde54dd730e1f
Fernando M Souza Filho <fernando.marques@ice.ufjf.br>
2017-12-01 04:42:03 -0200

    Adiciona elemento pai para facilitar nas buscas

--- XSDParser.py
+++ XSDParser.py
@@ -1,25 +1,25 @@
 from lxml import etree
 
 class XMLData(object):
-
-    ''' Objeto para servir de elemento na árvore que representa o XSD'''
+    """ Objeto para servir de elemento na arvore que representa o XSD """
     def __init__(self, tag_name):
         self.tag_name = tag_name
-        self.max_occurs = 0 #escolher default
-        self.min_occurs = 0 #escolher default
+        self.max_occurs = 0  #escolher default
+        self.min_occurs = 0  #escolher default
         self.type = ''
         self.name = ''
         self.path = ''
 
 
 class Node(object):
-    ''' Elemento estrutural da árvore '''
+    """ Elemento estrutural da arvore """
 
-    def __init__(self, tree, root):
+    def __init__(self, tree, root, father = None):
         self.children = []
+        self.father = father
         self.data = Node.set_data(tree, root)
         for child in root.getchildren():
-            self.children.append(Node(tree, child))
+            self.children.append(Node(tree, child, root))
 
     @staticmethod
     def set_data(tree, element):
